b6580d0333ffc90b9478e3d2e4e416475d599ce3
Letruke-007 <letruke@mail.ru>
2025-01-30 13:45:17 +0000

    fixed errors

--- src/sortingHeroesByHealth.js
+++ src/sortingHeroesByHealth.js
@@ -1,3 +1,3 @@
 export default function sortingHeroesByHealth(heroes) {
-    return heroes.sort((hero1, hero2) => hero2.health - hero1.health);
-  }
+  return heroes.sort((hero1, hero2) => hero2.health - hero1.health);
+}
